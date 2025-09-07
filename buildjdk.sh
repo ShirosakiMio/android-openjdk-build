05975a27fd325f096ffbff1c8cee1b72fac46c5d
ShirosakiMio <852468399@qq.com>
2025-09-07 20:23:29 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -61,7 +61,7 @@ if [[ "$TARGET_JDK" == "x86" ]]; then
 fi
 
 #添加shenandoahGC
-chmod +x ../shenandoahGC.tar.bz2.1 ../shenandoahGC.tar.bz2.2
+chmod +x ../patches/shenandoahGC.tar.bz2.1 ../patches/shenandoahGC.tar.bz2.2
 cat ../patches/shenandoahGC.tar.bz2.1 ../patches/shenandoahGC.tar.bz2.2 > shenandoahGC.tar.bz2
 chmod +x shenandoahGC.tar.bz2
 tar -xpvj shenandoahGC.tar.bz2 -C ../patches
